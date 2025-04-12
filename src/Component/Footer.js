de7b332b5d1532604697f3da4d17970a00b76256
Sachin Sharma <117962612+sachinxsharma@users.noreply.github.com>
2025-04-12 16:33:37 +0530

    Update Footer.js

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -28,9 +28,15 @@ const Footer = () => {
                 <h4>About Me </h4>
                 <p>professional with a strong foundation in  HTML, CSS, React.js and Java(frontend development). Proficient in data structures and algorithms, and pursuing a BSC/IT Degree from Somaiya VidyaVihar University to further enhance my technical skills and knowledge. Committed to continuously improving and staying up-to-date with emerging technologies.</p>
                 <div className="social">
-                <FaLinkedin size ={20} style = {{ color:"#fff", marginRight:"2rem" }}/>  
-                <FaGithub size ={20} style = {{ color:"#fff", marginRight:"2rem" }}/>
-                <FaInstagram size ={20} style = {{ color:"#fff", marginRight:"2rem" }}/>
+                        <a href="https://www.linkedin.com/in/sachin-sharma-145546254/" target="_blank" rel="noopener noreferrer">
+                          <FaLinkedin size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+                        </a>
+                        <a href="https://github.com/sachinxsharma" target="_blank" rel="noopener noreferrer">
+                          <FaGithub size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+                        </a>
+                        <a href="https://www.instagram.com/sachinnn_72?igsh=Z3FrenoydnY0M2Nt" target="_blank" rel="noopener noreferrer">
+                          <FaInstagram size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+                        </a>
                 </div>
             </div>
         </div>
